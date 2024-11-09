5a02907e0010034eda10ea3de00abe574bf323c3
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2024-11-09 14:43:50 +0000

    Add caching of forms

--- client_code/services/routes.py
+++ client_code/services/routes.py
@@ -1,4 +1,6 @@
-from routing.router import Route, Redirect
+from routing.router import Redirect, Route
+
+Route.cache_form = True
 
 
 class HomeRoute(Route):
@@ -12,7 +14,13 @@ class BookRoute(Route):
     path = "/books"
     form = "forms.pages.book.Index"
 
+    def cache_deps(self, **loader_args):
+        return None
+
 
 class AuthorRoute(Route):
     path = "/authors"
     form = "forms.pages.author.Index"
+
+    def cache_deps(self, **loader_args):
+        return None
