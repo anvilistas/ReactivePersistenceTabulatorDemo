4e6abfd0a5f24e20e12a9ded14245c6870f3eaae
stu@anvil.works <stu@anvil.works>
2024-11-12 23:39:52 +0800

    Edited forms.pages.book.Detail

--- client_code/forms/pages/book/Detail/__init__.py
+++ client_code/forms/pages/book/Detail/__init__.py
@@ -13,12 +13,12 @@ class Detail(DetailTemplate):
         self.actions_panel.store = globals.stores["book"]
         self.item = _model() if item is None else item
         self.actions_panel.item = self.item
-        bind(
-            self.author_dropdown,
-            "items",
-            globals.stores["author"],
-            "dropdown_items",
-        )
+        # bind(
+        #     self.author_dropdown,
+        #     "items",
+        #     globals.stores["author"],
+        #     "dropdown_items",
+        # )
         writeback(self.isbn_text_box, "text", self.item, "isbn_13", events=["change"])
         writeback(self.title_text_box, "text", self.item, "title", events=["change"])
         writeback(
@@ -31,40 +31,40 @@ class Detail(DetailTemplate):
             "published_on",
             events=["change"],
         )
-        writeback(
-            self.author_dropdown,
-            "selected_value",
-            self.item,
-            "author",
-            events=["change"],
-        )
+        # writeback(
+        #     self.author_dropdown,
+        #     "selected_value",
+        #     self.item,
+        #     "author",
+        #     events=["change"],
+        # )
         # self.author_dropdown.items = globals.stores["author"].dropdown_items
         # self.author_dropdown.selected_value = self.item["author"]
         self.init_components(**properties)
 
-    # @effect
-    # def render_1(self):
-    #     print("RENDER 1")
-    #     self.author_dropdown.items = globals.stores["author"].dropdown_items
-    #     print(globals.stores["author"].dropdown_items)
-    #     print("RENDER 1 DONE")
+    @effect
+    def render_1(self):
+        print("RENDER 1")
+        self.author_dropdown.items = globals.stores["author"].dropdown_items
+        print(globals.stores["author"].dropdown_items)
+        print("RENDER 1 DONE")
         
-    #     # print(self.item["author"], self.author_dropdown.selected_value)
+        # print(self.item["author"], self.author_dropdown.selected_value)
 
-    # @effect
-    # def render_2(self):
-    #     print("RENDER 2")
-    #     self.author_dropdown.selected_value = self.item["author"]
-    #     print(self.item["author"])
-    #     print("RENDER 2 DONE")
-    #     # print(self.item["author"], self.author_dropdown.selected_value)
+    @effect
+    def render_2(self):
+        print("RENDER 2")
+        self.author_dropdown.selected_value = self.item["author"]
+        print(self.item["author"])
+        print("RENDER 2 DONE")
+        # print(self.item["author"], self.author_dropdown.selected_value)
 
-    # @effect
-    # def render_3(self):
-    #     print("RENDER 3")
-    #     print(self.item["author"], self.author_dropdown.selected_value)
-    #     print("RENDER 3 DONE")
+    @effect
+    def render_3(self):
+        print("RENDER 3")
+        print(self.item["author"], self.author_dropdown.selected_value)
+        print("RENDER 3 DONE")
 
-    # def form_show(self, **event_args):
-    #     """This method is called when the form is shown on the page"""
-    #     print("SHOW")
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
+        print("SHOW")
