f9bb5c1dd2540009fcff99d194d227a885979066
Tom Campbell <thomas.campbell@empiria.co.uk>
2024-11-19 14:32:00 +0000

    Fix dependencies for author dropdown

--- client_code/startup.py
+++ client_code/startup.py
@@ -1,5 +1,5 @@
-from routing import router
-from app.services import routes  # noqa unused_import
 from app import globals  # noqa unused_import
+from app.services import routes  # noqa unused_import
+from routing import router
 
 router.launch()
